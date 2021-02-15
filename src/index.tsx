335c993ca8f01e84c50e36e0ff0cf999198f4b72
itaym <itaymer@gmail.com>
2021-02-15 08:31:06 +0200

    Typescript corrections

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import { ITabIndexer, IHashTable } from "./module";
+import { ITabIndexer, IHashTable, checkerFunction } from "./module";
 
 const tabIndexer = function ():ITabIndexer {
 
     const contexts:IHashTable = {}
     const numerators:IHashTable = {}
-    const checkerFunction:Function = function () { return true }
-    let checker:Function = checkerFunction
+    const checkerFunction:() => boolean = function () { return true }
+    let checker: (context: string, currentValue: number, numerator: number) => boolean|number = checkerFunction
 
     const indexer = (strings:TemplateStringsArray, num?:number, numerator?:number):number => {
         const context:string = strings.join('')
@@ -20,15 +20,15 @@ const tabIndexer = function ():ITabIndexer {
             return contexts[context] = num
         }
         const checkerResult = checker(context, contexts[context], numerators[context])
-        if (!isNaN(checkerResult) && checkerResult < 0) {
-            return checkerResult
+        if (!isNaN(checkerResult as number) && checkerResult < 0) {
+            return checkerResult as number
         }
         if (checkerResult !== 0 && !checkerResult) {
             return -1
         }
         return contexts[context] += numerators[context]
     }
-    indexer.setChecker = function (fn:Function):void {
+    indexer.setChecker = function (fn:checkerFunction):void {
         checker = fn
     }
     indexer.clearChecker = function ():void {
